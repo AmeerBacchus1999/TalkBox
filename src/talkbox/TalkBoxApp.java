a819046ba9a6e0fa2232dec865f3aafa022041d6
Mike <michaeldrrc@gmail.com>
2019-01-28 13:23:17 -0500

    Added files

--- src/talkbox/TalkBoxApp.java
+++ src/talkbox/TalkBoxApp.java
@@ -4,9 +4,11 @@ public class TalkBoxApp {
 
 	public static void main(String[] args) {
 	
+		/*
 		TalkBoxFrame frame = new TalkBoxFrame();
 		frame.pack();
 		frame.setVisible(true);
+		*/
 		
 	}
 }
