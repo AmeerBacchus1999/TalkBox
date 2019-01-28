c70a109137cb202a93af9c6bb5011484e45318f9
Mike <michaeldrrc@gmail.com>
2019-01-28 11:25:41 -0500

    Add everything

---
+++ src/talkbox/TalkBoxPanel.java
@@ -0,0 +1,26 @@
+package talkbox;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class TalkBoxPanel extends JPanel {
+	
+	public final int WIDTH = 900;
+	public final int HEIGHT = 900;
+	private Color color;
+	
+	
+	public TalkBoxPanel() {
+		
+		super();
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		color = Color.RED;//this is not doing anything right now
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/talkbox/Test.java
@@ -0,0 +1,12 @@
+package talkbox;
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Fun with Git");
+		//Hey it's Neena
+		//Hello it's Ameer
+		//Test
+	}
+
+}
