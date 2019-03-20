da49d5f21c1ce3e13b715ee3f26b50845f8d5f2e
Mike <michaeldrrc@gmail.com>
2019-03-20 17:49:11 -0400

    Added testing files

---
+++ src/test/java/TalkBox/AudioSetTest.java
@@ -0,0 +1,14 @@
+package test.java.TalkBox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AudioSetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/TalkBox/SwapButtonTest.java
@@ -0,0 +1,14 @@
+package test.java.TalkBox;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SwapButtonTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
